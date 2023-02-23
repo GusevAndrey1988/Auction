a7651d929e190c84199b5d567b5797cb745967d9
Andrey <GusevAndrey1988@gmail.com>
2023-02-23 19:03:00 +0300

    upd Makefile, create slim app

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,24 @@
 
 declare(strict_types = 1);
 
-header('Content-Type: application/json');
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
 
-echo '{}';
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get(
+    '/',
+    function (
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        mixed $args
+    ): ResponseInterface {
+        $response->getBody()->write('{}');
+        return $response->withHeader('Content-type', 'application/json');
+    }
+);
+
+$app->run();
